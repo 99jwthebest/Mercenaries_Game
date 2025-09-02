5ddfead8d9bbc65c5b7cd527db9d0316f426b3ce
Joseph Hernandez <artistic.distinction@gmail.com>
2025-09-02 17:37:49 -0500

    SmallPushForCreatingMeleeComponent

---
+++ Source/Merc_Game/Private/Components/Merc_MeleeComponent.cpp
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/Merc_MeleeComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values for this component's properties
+UMerc_MeleeComponent::UMerc_MeleeComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMerc_MeleeComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UMerc_MeleeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UMerc_MeleeComponent::PerformMeleeAttack()
+{
+    if (!bCanAttack) return;
+
+    AActor* Owner = GetOwner();
+    if (!Owner) return;
+
+    FVector Start = Owner->GetActorLocation();
+    FVector ForwardVector = Owner->GetActorForwardVector();
+    FVector End = Start + ForwardVector * MeleeRange;
+
+    TArray<AActor*> IgnoredActors;
+    IgnoredActors.Add(Owner);
+
+    TArray<FHitResult> HitResults;
+
+    bool bHit = UKismetSystemLibrary::SphereTraceMulti(
+        GetWorld(),
+        Start,
+        End,
+        50.f, // radius of the melee sweep
+        UEngineTypes::ConvertToTraceType(ECC_GameTraceChannel1),
+        false,
+        IgnoredActors,
+        EDrawDebugTrace::ForOneFrame,
+        HitResults,
+        true
+    );
+
+    if (bHit)
+    {
+        for (auto& Hit : HitResults)
+        {
+            AActor* HitActor = Hit.GetActor();
+            if (HitActor)
+            {
+                // Apply damage
+                UGameplayStatics::ApplyDamage(HitActor, MeleeDamage, Owner->GetInstigatorController(), Owner, nullptr);
+            }
+        }
+    }
+
+    // Start cooldown
+    bCanAttack = false;
+    GetWorld()->GetTimerManager().SetTimerForNextTick([this]() { ResetAttackCooldown(); });
+}
+
+void UMerc_MeleeComponent::ResetAttackCooldown()
+{
+	bCanAttack = true;
+}
---
+++ Source/Merc_Game/Private/Components/Merc_MeleeComponent.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Merc_MeleeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UMerc_MeleeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UMerc_MeleeComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+public:
+	void PerformMeleeAttack();
+
+	// Damage amount for the melee attack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Melee")
+	float MeleeDamage = 25.0f;
+
+	// Range of the melee attack
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Melee")
+	float MeleeRange = 150.0f;
+
+	// Collision channel for the melee attack
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Melee")
+	//TEnumAsByte<ECollisionChannel> MeleeTraceChannel = ECC_Pawn;
+
+	 // Optional: attack cooldown
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Melee")
+	float AttackCooldown = 0.5f;
+
+private: 
+	bool bCanAttack = true;
+
+	void ResetAttackCooldown();
+
+};
